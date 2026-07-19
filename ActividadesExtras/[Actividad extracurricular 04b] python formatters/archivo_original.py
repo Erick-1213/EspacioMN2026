0448c266bc13b7759444403652d617066f620614
Erick-1213 <cuichanerick07@gmail.com>
2026-07-19 13:03:15 -0500

    Actividad Extracurricular - 04B

---
+++ ActividadesExtras/[Actividad extracurricular 04b] python formatters/archivo_original.py
@@ -0,0 +1,8 @@
+def suma(a,b):  return a+b
+def resta(a , b):
+ return a-b
+def operacion(a,b,op):
+ if op=="+":
+  return suma(a,b)
+ else:
+  return resta(a,b)
---
+++ ActividadesExtras/[Actividad extracurricular 04b] python formatters/archivo_yapf.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a - b
+
+
+def operacion(a, b, op):
+    if op == "+":
+        return suma(a, b)
+    else:
+        return resta(a, b)
